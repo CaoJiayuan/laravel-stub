fcbe6befd950ff67caa6a1944918a4a1471cb489
caojiayuan <cjy632258@hotmail.com>
2021-04-04 19:09:09 +0800

    update stub

--- src/StubServiceProvider.php
+++ src/StubServiceProvider.php
@@ -4,6 +4,7 @@ namespace Nerio\LaravelStub;
 
 use Illuminate\Support\ServiceProvider;
 use Nerio\LaravelStub\Commands\MakeCommand;
+use function CaoJiayuan\Utility\file_map as file_mapAlias;
 
 /**
  * @author caojiayuan
@@ -23,5 +24,28 @@ class StubServiceProvider extends ServiceProvider
         });
 
         $this->commands(['command.stub.make']);
+    }
+
+    public function boot()
+    {
+        $this->autoloadApps();
+    }
+
+    protected function autoloadApps()
+    {
+        $config = $this->app['config'];
+
+        $root = $config->get('stub.app_path');
+        file_mapAlias(base_path($root), function ($dir, \SplFileInfo $info, $isdir) use ($root) {
+
+            if ($isdir) {
+                $appName = $info->getBasename();
+                $serviceProvider = ucfirst($root) . "\\{$appName}\\{$appName}ServiceProvider";
+
+                if (class_exists($serviceProvider)) {
+                    $this->app->register($serviceProvider);
+                }
+            }
+        }, false);
     }
 }
